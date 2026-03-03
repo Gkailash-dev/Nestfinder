c8af1323759a5f6f9a153f026df95a365d56da1f
KAILASH KUMAR.G <gkailashkumar.2005@gmail.com>
2026-03-03 11:25:32 +0530

    port

--- src/main/java/com/example/demo/Controller/Controlleraddhouse.java
+++ src/main/java/com/example/demo/Controller/Controlleraddhouse.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://nestfinderfrontend.vercel.app")
 public class Controlleraddhouse {
     @Autowired
 
